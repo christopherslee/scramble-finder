dfa1383e89a2e8bd9de660f296d2a78a13626379
Christopher Lee <christopher.s.lee@gmail.com>
2014-01-01 16:52:04 -0500

    leverage that movement returns nil if out of bounds to shorten up traversal code

--- src/clojure_fu/core.clj
+++ src/clojure_fu/core.clj
@@ -15,23 +15,17 @@
   (if (= 0 (mod current_pos grid_width)) nil (dec current_pos)))
 
 (defn find-word [current_idx word grid grid_width grid_height visited]
-  (if (= 0 (count word))
-    true
-    (if (not (contains? visited current_idx))
-      (if (= (first word) (nth grid current_idx))
-        (cond
-          (if (north current_idx grid_width grid_height)
-            (find-word (north current_idx grid_width grid_height) (rest word) grid grid_width grid_height (conj visited current_idx))) true
-          (if (south current_idx grid_width grid_height)
-            (find-word (south current_idx grid_width grid_height) (rest word) grid grid_width grid_height (conj visited current_idx))) true
-          (if (east current_idx grid_width grid_height)
-            (find-word (east current_idx grid_width grid_height) (rest word) grid grid_width grid_height (conj visited current_idx))) true
-          (if (west current_idx grid_width grid_height)
-            (find-word (west current_idx grid_width grid_height) (rest word) grid grid_width grid_height (conj visited current_idx))) true
-          :else false)
-        false)
-      false)
-    ))
+  (cond
+    (nil? current_idx) false
+    (zero? (count word)) true
+    (true? (contains? visited current_idx)) false ; we've already visited this node
+    (not (= (first word) (nth grid current_idx))) false ; this node doesn't match the first letter
+    :else (cond
+      (find-word (north current_idx grid_width grid_height) (rest word) grid grid_width grid_height (conj visited current_idx)) true
+      (find-word (east current_idx grid_width grid_height) (rest word) grid grid_width grid_height (conj visited current_idx)) true
+      (find-word (south current_idx grid_width grid_height) (rest word) grid grid_width grid_height (conj visited current_idx)) true
+      (find-word (west current_idx grid_width grid_height) (rest word) grid grid_width grid_height (conj visited current_idx)) true
+      :else false)))
 
 (defn exists-in-grid
   "find a word in a grid by traversing N, S, E, W (no diagonals, no repeats)"
